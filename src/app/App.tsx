278cb9060570b755d9265e96aeca7e6c390c9e66
W1ld_P03n1x <W1ld.Ph03n1x@gmail.com>
2024-05-09 17:45:20 +0300

    add abcjs testing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,9 +2,21 @@ import React from "react";
 import "./styles/App.css";
 import abcjs from "abcjs";
 import "abcjs/abcjs-audio.css";
-
+import ABCJS from "../pages/TestABCJS";
 function App() {
-	return <div className="App"></div>;
+	const abcString = `X:1
+	Q:1/4=90
+	L:1/16
+	M:4/4
+	K:perc middle=f,,,
+	V:1
+	[B,,,!style=x!^D,]B,,,!style=x!^D,B,,,   [D,,!style=x!^D,]z!style=x!^D,D,, [B,,,!style=x!^D,]B,,,!style=x!^D,B,,,[D,,!style=x!^D,]z!style=x!^D,D,,|[B,,,!style=x!^D,]B,,,^D,B,,,[D,,^D,]z^D,z[B,,,^C,^D,]z[B,,,^C,^D,]z[D,,^C,^D,]B,,,[^C,^D,]z|[B,,,^D,]B,,,^D,B,,,[D,,^D,]z^D,D,,[B,,,^D,]B,,,^D,B,,,[D,,^D,]z^D,D,,|[B,,,^D,]B,,,^D,B,,,[D,,^D,]z^D,z[B,,,^C,^D,]z[B,,,^C,^D,]z[D,,^C,^D,]B,,,[^C,^D,]z|
+	`;
+	return (
+		<div className="App">
+			<ABCJS abcString={abcString} />
+		</div>
+	);
 }
 
 export default App;
